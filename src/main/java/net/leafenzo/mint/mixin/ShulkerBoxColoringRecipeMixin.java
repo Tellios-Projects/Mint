6d022bd0e96722da52270c2bf70672baaeb5d157
Leafenzo <leafenzo@gmail.com>
2023-08-10 18:47:52 -0600

    Shulker Box Coloring Recipe MIXIN
    should be pretty compatible with things too

--- src/main/java/net/leafenzo/mint/mixin/ShulkerBoxColoringRecipeMixin.java
+++ src/main/java/net/leafenzo/mint/mixin/ShulkerBoxColoringRecipeMixin.java
@@ -1,38 +1,55 @@
 package net.leafenzo.mint.mixin;
 
-
 import net.leafenzo.mint.block.ModBlocks;
 import net.leafenzo.mint.block.ModShulkerBoxBlock;
 import net.leafenzo.mint.util.ModDyeColor;
 import net.minecraft.block.Block;
 import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.inventory.RecipeInputInventory;
 import net.minecraft.item.DyeItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.ShulkerBoxColoringRecipe;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.util.DyeColor;
-import org.spongepowered.asm.mixin.Debug;
-import org.spongepowered.asm.mixin.Interface;
-import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Debug(export = true)
 @Mixin(ShulkerBoxColoringRecipe.class)
 public class ShulkerBoxColoringRecipeMixin {
+    @Inject(method = "craft", at = @At("TAIL"), cancellable = true)
+    private void craft(RecipeInputInventory recipeInputInventory, DynamicRegistryManager dynamicRegistryManager, CallbackInfoReturnable<ItemStack> cir) {
+        //Just copying most things over cuz I don't wanna capture or overwrite any locals
+        ItemStack itemStack = ItemStack.EMPTY;
+        DyeItem dyeItem = (DyeItem) Items.WHITE_DYE;
+        for (int i = 0; i < recipeInputInventory.size(); ++i) {
+            ItemStack itemStack2 = recipeInputInventory.getStack(i);
+            if (itemStack2.isEmpty()) continue;
+            Item item = itemStack2.getItem();
+            if (Block.getBlockFromItem(item) instanceof ModShulkerBoxBlock) {
+                itemStack = itemStack2;
+                continue;
+            }
+            if (!(item instanceof DyeItem)) continue;
+            dyeItem = (DyeItem)item;
+        }
 
-    //TODO, figure this out
-//    @Inject(method = "craft", at = @At("TAIL"), cancellable = true)
-//    private ItemStack itemStack3(ItemStack itemStack, DyeItem dyeItem) {
-//        ItemStack itemStack3 = ShulkerBoxBlock.getItemStack(dyeItem.getColor());
-//        if (itemStack.hasNbt()) {
-//            itemStack3.setNbt(itemStack.getNbt().copy());
-//        }
-//        return itemStack3;
-//    }
-//    @Redirect(method = "get", at = @At("TAIL"), cancellable = true) // in case there's a registration timing error
-//    private ItemStack itemStack3(ItemStack itemStack, DyeItem dyeItem) {
-//
-//    }
+        //Only change the return value if it's from one of the ModDeColors, this is for compatibility reasons with other future dye mods of ours
+        DyeColor ingredientColor = dyeItem.getColor();
+        for (DyeColor color : ModDyeColor.VALUES) {
+            if(color == ingredientColor) {
+                ItemStack itemStack3 = ModShulkerBoxBlock.getItemStack(ingredientColor);
+                if (itemStack.hasNbt()) {
+                    itemStack3.setNbt(itemStack.getNbt().copy());
+                }
+                cir.setReturnValue(itemStack3);
+            }
+        }
+    }
 }
