5fb91d41c34ab4e859f0acdf7869e69a582965f2
Leafenzo <leafenzo@gmail.com>
2023-08-21 08:33:06 -0600

    add dye combining recipes

--- src/main/java/net/leafenzo/mint/datageneration/ModRecipeProvider.java
+++ src/main/java/net/leafenzo/mint/datageneration/ModRecipeProvider.java
@@ -2,6 +2,7 @@ package net.leafenzo.mint.datageneration;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.leafenzo.mint.Super;
 import net.leafenzo.mint.block.ModBlocks;
 import net.leafenzo.mint.item.ModItems;
 import net.leafenzo.mint.recipe.ModRecipeSerializer;
@@ -24,12 +25,33 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         ShapelessRecipeJsonBuilder.create(reverseCategory, baseItem, 9).input(compactItem).group(reverseGroup).criterion(RecipeProvider.hasItem(compactItem), RecipeProvider.conditionsFromItem(compactItem)).offerTo(exporter, new Identifier(reverseId+"_from_"+compactingId));
         ShapedRecipeJsonBuilder.create(compactingCategory, compactItem).input(Character.valueOf('#'), baseItem).pattern("###").pattern("###").pattern("###").group(compactingGroup).criterion(RecipeProvider.hasItem(baseItem), RecipeProvider.conditionsFromItem(baseItem)).offerTo(exporter, new Identifier(compactingId+"_from_"+reverseId));
     }
-    public static void offerShapelessRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, Ingredient ingredientInput, RecipeCategory recipeCategory, int count) {
-        ShapelessRecipeJsonBuilder.create(recipeCategory, output, count).input(ingredientInput);
+
+
+    public static void offerDyeMixingRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input1, ItemConvertible input2, RecipeCategory recipeCategory, int count) {
+        Identifier outputId = Registries.ITEM.getId(output.asItem());
+        Identifier input1Id = Registries.ITEM.getId(input1.asItem());
+        Identifier input2Id = Registries.ITEM.getId(input2.asItem());
+
+        if(input1 != input2) {
+            ShapelessRecipeJsonBuilder.create(recipeCategory, output, count)
+                    .input(input1)
+                    .input(input2)
+                    .group(outputId.getPath())
+                    .criterion(FabricRecipeProvider.hasItem(input1), FabricRecipeProvider.conditionsFromItem(input1))
+                    .criterion(FabricRecipeProvider.hasItem(input2), FabricRecipeProvider.conditionsFromItem(input2))
+                    .offerTo(exporter, outputId.getPath() + "_from_" + input1Id.getPath() + "_and_" + input2Id.getPath());
+        }
+        else {
+            ShapelessRecipeJsonBuilder.create(recipeCategory, output, count)
+                    .input(input1)
+                    .input(input2)
+                    .group(outputId.getPath())
+                    .criterion(FabricRecipeProvider.hasItem(input1), FabricRecipeProvider.conditionsFromItem(input1))
+                    .offerTo(exporter, outputId.getPath() + "_from_" + input1Id.getPath() + "_and_" + input2Id.getPath());
+        }
     }
-    public static void offerShapelessRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible itemProvider, RecipeCategory recipeCategory, int count) {
-        ShapelessRecipeJsonBuilder.create(recipeCategory, output, count).input(itemProvider);
-    }
+
+
     public static void offerCarpetRecipes(Consumer<RecipeJsonProvider> exporter, Block[] carpets, Block[] inputs) {
         if(carpets.length > inputs.length) { throw new RuntimeException();
 //            throw new RuntimeException("More carpets than wools");
@@ -56,7 +78,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         if(outputs.length > inputs.length) { throw new RuntimeException(); }
         for(int i = 0; i < inputs.length; i++) {
         CookingRecipeJsonBuilder.createSmelting(Ingredient.ofItems(inputs[i].asItem()), RecipeCategory.DECORATIONS, outputs[i].asItem(), 0.1f, 200)
-                .criterion("has_" + Registries.ITEM.getId(inputs[i].asItem()), FabricRecipeProvider.conditionsFromItem(inputs[i].asItem()))
+                .criterion(FabricRecipeProvider.hasItem(inputs[i]), FabricRecipeProvider.conditionsFromItem(inputs[i].asItem()))
                 .offerTo(exporter);
         }
     }
@@ -109,8 +131,18 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                         FabricRecipeProvider.conditionsFromItem(ModItems.MINT_SPRIG))
                 .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.MINT_COOKIE) + "_shapeless"));
         offerShapelessRecipe(exporter, ModItems.MINT_DYE, ModItems.MINT_SPRIG, RecipeCategory.MISC.getName(), 1);
-        this.offerShapelessRecipe(exporter, ModItems.MINT_DYE, Ingredient.ofItems(Items.LIME_DYE, Items.LIGHT_BLUE_DYE), RecipeCategory.MISC, 2);
         this.offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, ModItems.MINT_SPRIG, RecipeCategory.BUILDING_BLOCKS, ModBlocks.MINT_SPRIG_BLOCK);
+
+        //Dyes from combining vanilla dyes
+        this.offerDyeMixingRecipe(exporter, ModItems.MINT_DYE, Items.LIME_DYE, Items.LIGHT_BLUE_DYE, RecipeCategory.MISC, 2);
+        this.offerDyeMixingRecipe(exporter, ModItems.PEACH_DYE, Items.PINK_DYE, Items.ORANGE_DYE, RecipeCategory.MISC, 2);
+        this.offerDyeMixingRecipe(exporter, ModItems.PERIWINKLE_DYE, Items.PURPLE_DYE, Items.LIGHT_BLUE_DYE, RecipeCategory.MISC, 2);
+        this.offerDyeMixingRecipe(exporter, ModItems.ARTICHOKE_DYE, Items.LIME_DYE, Items.YELLOW_DYE, RecipeCategory.MISC, 2);
+        this.offerDyeMixingRecipe(exporter, ModItems.FUCHSIA_DYE, Items.MAGENTA_DYE, Items.RED_DYE, RecipeCategory.MISC, 2);
+        this.offerDyeMixingRecipe(exporter, ModItems.VERMILION_DYE, Items.ORANGE_DYE, Items.RED_DYE, RecipeCategory.MISC, 2);
+        this.offerDyeMixingRecipe(exporter, ModItems.SHAMROCK_DYE, Items.LIME_DYE, Items.LIME_DYE, RecipeCategory.MISC, 2);
+        this.offerDyeMixingRecipe(exporter, ModItems.INDIGO_DYE, Items.BLUE_DYE, Items.PURPLE_DYE, RecipeCategory.MISC, 2);
+        this.offerDyeMixingRecipe(exporter, ModItems.BANANA_DYE, Items.YELLOW_DYE, Items.WHITE_DYE, RecipeCategory.MISC, 2);
 
         // Main
 //  WOOL_BLOCKS
