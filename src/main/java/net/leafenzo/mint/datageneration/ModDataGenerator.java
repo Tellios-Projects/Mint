55b6074cb5776c7d906be85121d459850aff122a
Leafenzo <leafenzo@gmail.com>
2024-04-01 14:43:16 -0600

    Revert "start new datagen packs"
    This reverts commit 240fcb7c123fe3b336b44b42c7bc4e47eeac8b19.

--- src/main/java/net/leafenzo/mint/datageneration/ModDataGenerator.java
+++ src/main/java/net/leafenzo/mint/datageneration/ModDataGenerator.java
@@ -6,15 +6,9 @@ import net.minecraft.registry.RegistryBuilder;
 import net.minecraft.registry.RegistryKeys;
 
 public class ModDataGenerator implements DataGeneratorEntrypoint {
-
-    // Using an accessor like this didn't work because of how the target class is final... I think?
-    public net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator.Pack createPack(net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator fabricDataGenerator, String forModId) {
-        return new net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator.Pack(true, forModId, ((FabricDataGenerator)(Object) fabricDataGenerator).getFabricOutput());
-    }
-
     @Override
-    public void onInitializeDataGenerator(net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator fabricDataGenerator) {
-        net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
         pack.addProvider(ModLootTableGenerator::new);
         pack.addProvider(ModRecipeProvider::new);
         pack.addProvider(ModBlockTagGenerator::new);
@@ -22,8 +16,6 @@ public class ModDataGenerator implements DataGeneratorEntrypoint {
         pack.addProvider(ModModelProvider::new);
         pack.addProvider(ModWorldGenerator::new);
         pack.addProvider(ModEnglishLangProvider::new);
-
-        net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator.Pack supplimentaries = fabricDataGenerator.;
     }
     @Override public void buildRegistry(RegistryBuilder registryBuilder) {
         registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
