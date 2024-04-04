05e75935b874a4efa67d088c5e6b16fff8b3da10
Leafenzo <leafenzo@gmail.com>
2024-04-03 21:58:59 -0600

    Update colorize.sh

--- tools/colorize.sh
+++ tools/colorize.sh
@@ -8,13 +8,11 @@ REPLACE="magenta"
 for f in $(find $TEMPLATES -type f -name "*.json"); do
   while read COLOR; do
     COLOR=$(echo $COLOR | tr -d "\r") # Remove the carriage returns
-
     NAME=$(sed "s/${REPLACE}/${COLOR}/g" <(echo $f | rev | cut -d "/" -f1 | rev))
     OUT_PATH="${OUT_DIRECTORY}"$(echo $f | rev | cut -d "/" -f2- | rev | cut -d "/" -f2-)
 
-    # echo $OUT_PATH/$NAME
     mkdir -p $OUT_PATH
-    sed "s/${REPLACE}/${COLOR}/g" $f > "${OUT_PATH}/${NAME}"
+    sed "s/${REPLACE}/${COLOR}/g" $f | sed "/minecraft:*${COLOR}/s/minecraft:/mint:/g" | sed "s/\"block\/${COLOR}/\"mint:block\/${COLOR}/g" > "${OUT_PATH}/${NAME}"
 
   done < $COLORLIST
 done
