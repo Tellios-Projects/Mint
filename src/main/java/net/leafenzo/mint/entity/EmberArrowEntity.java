9cac5bec129508c4b42867c4224494c6a5089a9b
Leafenzo <leafenzo@gmail.com>
2024-04-11 01:20:01 -0600

    Change emberArrow pickup
    so that if the goop is placed you just pick up a normal arrow

--- src/main/java/net/leafenzo/mint/entity/EmberArrowEntity.java
+++ src/main/java/net/leafenzo/mint/entity/EmberArrowEntity.java
@@ -7,9 +7,11 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.state.property.Properties;
@@ -38,15 +40,20 @@ public class EmberArrowEntity extends PersistentProjectileEntity {
     protected void onBlockHit(BlockHitResult blockHitResult) {
         World world = getWorld();
         BlockPos pos = blockHitResult.getBlockPos().offset(blockHitResult.getSide());
+//        if (!world.getBlockState(pos).isFullCube(world, pos)) { // if not a full cube. IE, we've hit a fence. oops nvm this doesn't work, hm... not sure if we should even worry about this, it's not very weird imo. - Leah
+//            super.onBlockHit(blockHitResult); return;
+//        }
         if (world.getBlockState(pos).isOf(ModBlocks.EMBER)) {
             BlockState prevState = world.getBlockState(pos);
             world.setBlockState(pos, ModBlocks.EMBER.getStateWithProperties(prevState).with(EmberBlock.getProperty(blockHitResult.getSide().getOpposite()), true));
+            hasPlacedEmber = false;
         } else if (world.getBlockState(pos).getFluidState().isEqualAndStill(Fluids.WATER)) {
             world.setBlockState(pos, ModBlocks.EMBER.getDefaultState().with(EmberBlock.getProperty(blockHitResult.getSide().getOpposite()), true).with(EmberBlock.WATERLOGGED, true));
+            hasPlacedEmber = true;
         } else if (world.getBlockState(pos).isReplaceable()) {
             world.setBlockState(pos, ModBlocks.EMBER.getDefaultState().with(EmberBlock.getProperty(blockHitResult.getSide().getOpposite()), true));
+            hasPlacedEmber = true;
         }
-        hasPlacedEmber = true;
         super.onBlockHit(blockHitResult);
     }
 
@@ -62,6 +69,26 @@ public class EmberArrowEntity extends PersistentProjectileEntity {
 
     protected ItemStack asItemStack() {
         return new ItemStack(ModItems.EMBER_ARROW);
+    }
+
+    @Override
+    protected boolean tryPickup(PlayerEntity player) {
+        switch (this.pickupType) {
+            case ALLOWED -> {
+                if(hasPlacedEmber) {
+                    return player.getInventory().insertStack(Items.ARROW.getDefaultStack()); // Ember goop comes off the arrow, leaving a regular arrow
+                }
+                else {
+                    return player.getInventory().insertStack(this.asItemStack());
+                }
+            }
+            case CREATIVE_ONLY -> {
+                return player.getAbilities().creativeMode;
+            }
+            default -> {
+                return false;
+            }
+        }
     }
 
     protected void onHit(LivingEntity target) {
