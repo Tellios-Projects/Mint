86151169617b60e98adbbb2913639a923688753d
Leafenzo <leafenzo@gmail.com>
2024-06-11 05:30:24 -0600

    Update ModDyeColor.java
    just some notes, ill come back to this a little bit later

--- src/main/java/net/leafenzo/mint/util/ModDyeColor.java
+++ src/main/java/net/leafenzo/mint/util/ModDyeColor.java
@@ -3,7 +3,7 @@ package net.leafenzo.mint.util;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.block.MapColor;
 import net.minecraft.util.DyeColor;
-import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.function.ValueLists;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
@@ -13,23 +13,46 @@ import java.util.Map;
 import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 
-public enum ModDyeColor implements StringIdentifiable {
-    WHITE(0, "white", 16383998, MapColor.WHITE, 15790320, 16777215),
-    ORANGE(1, "orange", 16351261, MapColor.ORANGE, 15435844, 16738335),
-    MAGENTA(2, "magenta", 13061821, MapColor.MAGENTA, 12801229, 16711935),
-    LIGHT_BLUE(3, "light_blue", 3847130, MapColor.LIGHT_BLUE, 6719955, 10141901),
-    YELLOW(4, "yellow", 16701501, MapColor.YELLOW, 14602026, 16776960),
-    LIME(5, "lime", 8439583, MapColor.LIME, 4312372, 12582656),
-    PINK(6, "pink", 15961002, MapColor.PINK, 14188952, 16738740),
-    GRAY(7, "gray", 4673362, MapColor.GRAY, 4408131, 8421504),
-    LIGHT_GRAY(8, "light_gray", 10329495, MapColor.LIGHT_GRAY, 11250603, 13882323),
-    CYAN(9, "cyan", 1481884, MapColor.CYAN, 2651799, 65535),
-    PURPLE(10, "purple", 8991416, MapColor.PURPLE, 8073150, 10494192),
-    BLUE(11, "blue", 3949738, MapColor.BLUE, 2437522, 255),
-    BROWN(12, "brown", 8606770, MapColor.BROWN, 5320730, 9127187),
-    GREEN(13, "green", 6192150, MapColor.GREEN, 3887386, 65280),
-    RED(14, "red", 11546150, MapColor.RED, 11743532, 16711680),
-    BLACK(15, "black", 1908001, MapColor.BLACK, 1973019, 0);
+public class ModDyeColor {
+
+    // What the heck do I even use here?
+    // I need an expandable list that can be treated as an enum, I surely couldn't just instance the DyeColor enum, right?
+    // Maybe I can... that would be the easiest solution, I should start with that and hope it works.
+
+//    <DyeColor> VALUES = new ArrayList<DyeColor>();
+
+//    WHITE(0, "white", 16383998, MapColor.WHITE, 15790320, 16777215),
+//    ORANGE(1, "orange", 16351261, MapColor.ORANGE, 15435844, 16738335),
+//    MAGENTA(2, "magenta", 13061821, MapColor.MAGENTA, 12801229, 16711935),
+//    LIGHT_BLUE(3, "light_blue", 3847130, MapColor.LIGHT_BLUE, 6719955, 10141901),
+//    YELLOW(4, "yellow", 16701501, MapColor.YELLOW, 14602026, 16776960),
+//    LIME(5, "lime", 8439583, MapColor.LIME, 4312372, 12582656),
+//    PINK(6, "pink", 15961002, MapColor.PINK, 14188952, 16738740),
+//    GRAY(7, "gray", 4673362, MapColor.GRAY, 4408131, 8421504),
+//    LIGHT_GRAY(8, "light_gray", 10329495, MapColor.LIGHT_GRAY, 11250603, 13882323),
+//    CYAN(9, "cyan", 1481884, MapColor.CYAN, 2651799, 65535),
+//    PURPLE(10, "purple", 8991416, MapColor.PURPLE, 8073150, 10494192),
+//    BLUE(11, "blue", 3949738, MapColor.BLUE, 2437522, 255),
+//    BROWN(12, "brown", 8606770, MapColor.BROWN, 5320730, 9127187),
+//    GREEN(13, "green", 6192150, MapColor.GREEN, 3887386, 65280),
+//    RED(14, "red", 11546150, MapColor.RED, 11743532, 16711680),
+//    BLACK(15, "black", 1908001, MapColor.BLACK, 1973019, 0);
+
+//    private ModDyeColor(int id, String name, int color, MapColor mapColor, int fireworkColor, int signColor) {
+//        this.id = id;
+//        this.name = name;
+//        this.mapColor = mapColor;
+//        this.signColor = signColor;
+//        int j = (color & 16711680) >> 16;
+//        int k = (color & '\uff00') >> 8;
+//        int l = (color & 255) >> 0;
+//        this.colorComponents = new float[]{(float)j / 255.0F, (float)k / 255.0F, (float)l / 255.0F};
+//        this.fireworkColor = fireworkColor;
+//    }
+
+    public DyeColor registerDyeColor(Identifier identifier, int color, MapColor mapColor, int fireworkColor, int signColor) {
+
+    }
 
     private static final IntFunction<DyeColor> BY_ID = ValueLists.createIdToValueFunction(DyeColor::getId, values(), ValueLists.OutOfBoundsHandling.ZERO);
     private static final Int2ObjectOpenHashMap<DyeColor> BY_FIREWORK_COLOR = new Int2ObjectOpenHashMap((Map) Arrays.stream(values()).collect(Collectors.toMap((color) -> {
@@ -37,25 +60,14 @@ public enum ModDyeColor implements StringIdentifiable {
     }, (color) -> {
         return color;
     })));
-    public static final StringIdentifiable.Codec<DyeColor> CODEC = StringIdentifiable.createCodec(DyeColor::values);
-    private final int id;
-    private final String name;
-    private final MapColor mapColor;
-    private final float[] colorComponents;
-    private final int fireworkColor;
-    private final int signColor;
+//    public static final StringIdentifiable.Codec<DyeColor> CODEC = StringIdentifiable.createCodec(DyeColor::values);
+//    private final int id;
+//    private final String name;
+//    private final MapColor mapColor;
+//    private final float[] colorComponents;
+//    private final int fireworkColor;
+//    private final int signColor;
 
-    private DyeColor(int id, String name, int color, MapColor mapColor, int fireworkColor, int signColor) {
-        this.id = id;
-        this.name = name;
-        this.mapColor = mapColor;
-        this.signColor = signColor;
-        int j = (color & 16711680) >> 16;
-        int k = (color & '\uff00') >> 8;
-        int l = (color & 255) >> 0;
-        this.colorComponents = new float[]{(float)j / 255.0F, (float)k / 255.0F, (float)l / 255.0F};
-        this.fireworkColor = fireworkColor;
-    }
 
     public int getId() {
         return this.id;
